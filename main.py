1421d2e9dd96da47d487d29a5313c24e3e5cbbaf
Stan <79027654+StanSmits@users.noreply.github.com>
2021-10-13 00:40:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,18 @@ X1, Y1 = (1448 / 1920) * screensize[0], (152 / 1080) * screensize[1]
 X2, Y2 = (1512 / 1920) * screensize[0], (242 / 1080) * screensize[1]
 X3, Y3 = (1081 / 1920) * screensize[0], (578 / 1080) * screensize[1]
 
-time.sleep(5)
-print("Starting the script in 5 seconds")
-for i in range(amountOfFriends):
-    pyautogui.click(X1, Y1)
-    time.sleep(0.2)
 
-    pyautogui.click(X2, Y2)
-    time.sleep(0.3)
+if amountOfFriends = 0:
+    print("Make sure you have changed all the settings in the script itself", "\nTo do this you need to open the file in a text editor, and change \"amountOfFriends\" to the amount of friends you wanna remove / have")
+else:
+    time.sleep(5)
+    print("Starting the script in 5 seconds, you can only use your mouse when the entire process is done")
+    for i in range(amountOfFriends):
+        pyautogui.click(X1, Y1)
+        time.sleep(0.2)
 
-    pyautogui.click(X3, Y3)
-    time.sleep(0.5)
+        pyautogui.click(X2, Y2)
+        time.sleep(0.3)
+
+        pyautogui.click(X3, Y3)
+        time.sleep(0.5)
